b998f0bb5c7221f1b77b80836d939d3e71bfd70a
Ryan Kari <RKari@magcanica.com>
2021-02-05 10:07:00 -0800

    Updated RDS endpoint

--- config.py
+++ config.py
@@ -2,7 +2,8 @@
 # The other elements are the same as used in the tutorial
 # format: (user):(password)@(db_identifier).amazonaws.com:3306/(db_name)
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb7ywmi.us-east-1.rds.amazonaws.com:3306/flaskdemo'
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://aws_rkari:newpassword@flasktest.cryxwnwcbmqt.us-west-1.rds.amazonaws.com:3306/flasktestdb'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://aws_rkari:newpassword@https://584126383582.signin.aws.amazon.com/console:3306/flasktestdb'
 
 # Uncomment the line below if you want to work with a local DB
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
@@ -10,4 +11,4 @@ SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://flaskdemo:flaskdemo@flaskdemo.cwsaehb
 SQLALCHEMY_POOL_RECYCLE = 3600
 
 WTF_CSRF_ENABLED = True
-SECRET_KEY = 'dsaf0897sfdg45sfdgfdsaqzdf98sdf0a'
+SECRET_KEY = 'L2gkGvpyezc3EqrbJNGYY3B4Ew+mVK6x+a69PTnU'
